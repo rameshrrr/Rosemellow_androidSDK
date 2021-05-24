763b20e1c64fd2f51b36efadcd04ac6a117bdd9f
Ramesh <rameshrp16@gmail.com>
2021-05-24 12:24:01 +0530

    Create Project in Studio. Create LogDebug for easy debugging.

---
+++ app/src/main/java/com/efficienza/samplesdk/Register.java
@@ -0,0 +1,81 @@
+package com.efficienza.samplesdk;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Register extends AppCompatActivity {
+
+    private ImageView logo, joinus;
+    private AutoCompleteTextView username, email, password;
+    private Button signup;
+    private TextView signin;
+    private ProgressDialog progressDialog;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        initializeGUI();
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                final String inputName = username.getText().toString().trim();
+                final String inputPw = password.getText().toString().trim();
+                final String inputEmail = email.getText().toString().trim();
+
+                if(validateInput(inputName, inputPw, inputEmail));
+
+
+            }
+        });
+
+
+
+
+    }
+
+
+    private void initializeGUI(){
+
+        logo = findViewById(R.id.ivRegLogo);
+        username = findViewById(R.id.atvUsernameReg);
+        email =  findViewById(R.id.atvEmailReg);
+        password = findViewById(R.id.atvPasswordReg);
+        signup = findViewById(R.id.btnSignUp);
+        progressDialog = new ProgressDialog(this);
+
+
+    }
+
+
+
+    private boolean validateInput(String inName, String inPw, String inEmail){
+
+        if(inName.isEmpty()){
+            username.setError("Username is empty.");
+            return false;
+        }
+        if(inPw.isEmpty()){
+            password.setError("Password is empty.");
+            return false;
+        }
+        if(inEmail.isEmpty()){
+            email.setError("Email is empty.");
+            return false;
+        }
+
+        return true;
+    }
+
+
+}
---
+++ rosemellowSDK/src/main/java/com/efficienza/rosemellowsdk/Rosemellow_register.java
@@ -0,0 +1,14 @@
+package com.efficienza.rosemellowsdk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Rosemellow_register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rosemellow_register);
+    }
+}
